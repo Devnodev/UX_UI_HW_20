3910124aa02a5264a1b391fdfa204aa78fa8a323
Devnodev <70086256+Devnodev@users.noreply.github.com>
2021-02-02 23:44:21 -0500

    CAse study update

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+$('#process').on('click', function(){
+    $('#process').css('background-color', '#52616B');
+});
+$('h3').on('click', function(){
+    $('h3').css('color', '#52616B');
+});
+
+
+
+
